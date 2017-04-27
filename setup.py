634efd42474fd945d0d5ed792ad74e44301019f1
crazyzubr <hjcnbckfd@gmail.com>
2017-04-27 12:34:20 +0300

    Normalize package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='Django Reverse Admin',
+    name='django-reverse-admin',
     version='0.0.1',
     packages=['django_reverse_admin',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
